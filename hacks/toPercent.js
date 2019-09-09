8a5683479c4f85fa2cb5a76053943d2ae7f612a7
wfatec <chao125010@163.com>
2019-09-09 17:36:59 +0800

    update toPercent

--- hacks/toPercent.js
+++ hacks/toPercent.js
@@ -1,6 +1,8 @@
-export const toPercent = (point = 0, accuracy = 2) => {
+export const toPercent1 = (point = 0, accuracy = 2) => {
   let str =
-    Math.floor(Number(point * 100 * Math.pow(10, accuracy))) /
+    Math.floor(
+      Number(point * 100 * Math.pow(10, accuracy)).toFixed(accuracy + 2)
+    ) /
       Math.pow(10, accuracy) +
     "";
   let [intPart, decPart = ""] = str.split(".");
@@ -9,4 +11,25 @@ export const toPercent = (point = 0, accuracy = 2) => {
     decPart += "0";
   }
   return intPart + "." + decPart + "%";
+};
+
+export const toPercent = (point = 0, accuracy = 2) => {
+  const pointStr = point + "";
+  const [intPart, decimalPart = ""] = pointStr.split(".");
+  const intPartArray = intPart.split("");
+  const dcimalPartArray = decimalPart.split("");
+
+  for (let index = 0; index < 2; index++) {
+    intPartArray.push(dcimalPartArray.shift() || "0");
+  }
+
+  let len = dcimalPartArray.length;
+  for (len; len < accuracy; len++) {
+    dcimalPartArray.push("0");
+  }
+  dcimalPartArray.splice(accuracy);
+
+  const intPartFinal = +intPartArray.join("");
+  const dcimalPartFinal = dcimalPartArray.join("");
+  return intPartFinal + "." + dcimalPartFinal + "%";
 };
