fda0f3eb7f6874aaacba7e9bb1cefdc314bad640
wfatec <chao125010@163.com>
2019-09-09 15:56:17 +0800

    update  toPercent to accept accuracy

--- hacks/toPercent.js
+++ hacks/toPercent.js
@@ -1,12 +1,12 @@
-export const toPercent = point => {
-  if (!point) {
-    return "0.00%";
-  }
-  let str = Math.floor(Number(point * 10000)) / 100 + "";
-  let [intPart, decPart = "00"] = str.split(".");
+export const toPercent = (point = 0, accuracy = 2) => {
+  let str =
+    Math.floor(Number(point * 100 * Math.pow(10, accuracy))) /
+      Math.pow(10, accuracy) +
+    "";
+  let [intPart, decPart = ""] = str.split(".");
   let len = decPart.length;
-  for (len; len < 2; len++) {
+  for (len; len < accuracy; len++) {
     decPart += "0";
   }
   return intPart + "." + decPart + "%";
-}
+};
